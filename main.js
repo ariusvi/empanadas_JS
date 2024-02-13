30532d5a3fe2b017abdfe7a212ddf80d145d3b3b
Ana Rius <a.riusvi11@gmail.com>
2024-02-13 16:24:18 +0100

    creación de validaciones -menor que 40- y -multiplo de 3-

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
-export const fn = (/* argumentos de la función */) => {
+export const fn = (a, b, c) => {
    // Cuerpo de la función: Completa la lógica de la función aquí.
 };
+
+
+const a = 12;     /*precio empanada pollo*/
+const b = 14;     /*precio empanada ternera*/
+const c = 16;     /*precio empanada verdura*/
+
+
+
+if (a < 0 && b < 0 && c < 0) {                                       /* validar que a, b y c, son números enteros*/
+   throw new Error("Introduzca una cantidad de empanadas válida");
+}
+
+if ((a + b + c) >= 40) {                                             /* validar que el total de empanadas no sea mayor que 40*/
+   throw new Error("Excede la cantidad máxima de empanadas");
+}
+
+if ((a+b+c) % 3 !=0) {                                               /*validar que las empanadas sean múltiplo de 3*/
+   throw new Error("No se aplica oferta 3x1");
+}
