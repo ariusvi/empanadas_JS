6f3c68d15c7f39e5a1c0c7dc9c9866ede6d1f1ec
Ana Rius <a.riusvi11@gmail.com>
2024-02-14 09:55:12 +0100

    inresercción de operación matemática para resolver el problema

--- main.js
+++ main.js
@@ -1,22 +1,28 @@
 export const fn = (a, b, c) => {
-   // Cuerpo de la función: Completa la lógica de la función aquí.
+
+   const precioA = 12;     /*precio empanada pollo*/
+   const precioB = 14;     /*precio empanada ternera*/
+   const precioC = 16;     /*precio empanada verdura*/
+
+   if (a < 0 || b < 0 || c < 0) {                                                /* validar que a, b y c, son números enteros, se utiliza | y no &, para que salte error solo con que 1 de los valores sea menor que 0, incluyendo así los números negativos*/
+      throw new Error("Introduzca una cantidad de empanadas válida");
+   } 
+   
+   else if ((a + b + c) >= 40) {                                                 /* validar que el total de empanadas no sea mayor que 40*/
+      throw new Error("Excede la cantidad máxima de empanadas");
+   }
+
+   else if ((a + b + c) % 3 != 0) {                                               /*validar que las empanadas sean múltiplo de 3*/
+      throw new Error("No se aplica oferta 3x1");
+   }
+   else {
+       let salida = ((a * precioA) + (b * precioB) + (c * precioC)) / 3;         /*operacion matemática para mostrar la salida dado 3 valores*/
+   //faltaria sacar por pantalla el resultado y redondearlo a la alza
+   
+
+
 };
 
 
-const a = 12;     /*precio empanada pollo*/
-const b = 14;     /*precio empanada ternera*/
-const c = 16;     /*precio empanada verdura*/
 
-
-
-if (a < 0 && b < 0 && c < 0) {                                       /* validar que a, b y c, son números enteros*/
-   throw new Error("Introduzca una cantidad de empanadas válida");
-}
-
-if ((a + b + c) >= 40) {                                             /* validar que el total de empanadas no sea mayor que 40*/
-   throw new Error("Excede la cantidad máxima de empanadas");
-}
-
-if ((a+b+c) % 3 !=0) {                                               /*validar que las empanadas sean múltiplo de 3*/
-   throw new Error("No se aplica oferta 3x1");
 }
