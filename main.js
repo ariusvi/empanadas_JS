9fbc992c2d500016db55a6a40d8c39444b8f61d5
Ana Rius <a.riusvi11@gmail.com>
2024-02-14 17:54:38 +0100

    finalización del ejercicio, se pasan todos los test de forma satisfactoria

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ export const fn = (a, b, c) => {
 
    if (a < 0 || b < 0 || c < 0) {                                                /* validar que a, b y c, son números enteros, se utiliza | y no &, para que salte error solo con que 1 de los valores sea menor que 0, incluyendo así los números negativos*/
       throw new Error("Introduzca una cantidad de empanadas válida");
-   } 
-   
+   }
+
    else if ((a + b + c) >= 40) {                                                 /* validar que el total de empanadas no sea mayor que 40*/
       throw new Error("Excede la cantidad máxima de empanadas");
    }
@@ -16,13 +16,9 @@ export const fn = (a, b, c) => {
       throw new Error("No se aplica oferta 3x1");
    }
    else {
-       let salida = ((a * precioA) + (b * precioB) + (c * precioC)) / 3;         /*operacion matemática para mostrar la salida dado 3 valores*/
-   //faltaria sacar por pantalla el resultado y redondearlo a la alza
-   
+      let salida = Math.ceil(((a * precioA) + (b * precioB) + (c * precioC)) / 3);         /*operacion matemática para mostrar la salida dado 3 valores*/
 
-
-};
-
-
+      return salida;                                                                         /*con math.ceil se redondea a la alza, buscado en google*/
+   };
 
 }
